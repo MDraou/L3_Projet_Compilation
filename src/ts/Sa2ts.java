aa987424a870885877674c05e3ee98194344b7c9
s19000780 <julien.sourigues@etu.univ-amu.fr>
2022-03-22 18:44:09 +0100

    début de sa2ts/tp3, visit de SaAppel et visit de SaDecFonc sont fait mais pas tester.

--- src/ts/Sa2ts.java
+++ src/ts/Sa2ts.java
@@ -15,7 +15,7 @@ public class Sa2ts extends SaDepthFirstVisitor <Void> {
 
     public Sa2ts(SaNode node) {
         this.tableGlobale = new Ts();
-        this.tableGlobale = new Ts();
+        this.tableLocaleCourante = new Ts();
         this.context = Context.GLOBAL;
         node.accept(this);
     }
@@ -42,9 +42,26 @@ public class Sa2ts extends SaDepthFirstVisitor <Void> {
     public Void visit(SaDecFonc node)
     {
         defaultIn(node);
+        boolean parametresIsNull = false;
+        this.tableLocaleCourante = new Ts();
+        this.context = Context.PARAM;
         if(node.getParametres() != null) node.getParametres().accept(this);
+        else parametresIsNull = true;
+        this.context = Context.LOCAL;
         if(node.getVariable() != null) node.getVariable().accept(this);
+
+        if (tableGlobale.getFct(node.getNom()) == null) {
+            if (parametresIsNull) node.tsItem = tableGlobale.addFct(node.getNom(), 0, tableLocaleCourante, node);
+            else
+                node.tsItem = tableGlobale.addFct(node.getNom(), node.getParametres().length(), tableLocaleCourante, node);
+        }
+        else {
+            System.out.println("La fonction " + node.getNom() + " est déjà déclarée autre part");
+            System.exit(2);
+        }
         if(node.getCorps() != null) node.getCorps().accept(this);
+        this.context = Context.GLOBAL;
+        tableLocaleCourante = null;
         defaultOut(node);
         return null;
     }
@@ -70,7 +87,32 @@ public class Sa2ts extends SaDepthFirstVisitor <Void> {
     public Void visit(SaAppel node)
     {
         defaultIn(node);
-        if(node.getArguments() != null) node.getArguments().accept(this);
+        if (tableGlobale.getFct(node.getNom()) != null){
+            if (tableGlobale.getFct("main") != null) {
+                if (tableGlobale.getFct(node.getNom()).saDecFonc.getParametres() != null) {
+                    if (node.getArguments().length() == tableGlobale.getFct(node.getNom()).saDecFonc.getParametres().length()) {
+                        node.tsItem = tableGlobale.getFct(node.getNom());
+                    } else {
+                        System.out.println("La fonction" + node.getNom() + "n'as pas le bon nombre d'arguments");
+                        System.exit(2);
+                    }
+                } else {
+                    if (node.getArguments().length() == 0) {
+                        node.tsItem = tableGlobale.getFct(node.getNom());
+                    } else {
+                        System.out.println("La fonction" + node.getNom() + "n'as pas le bon nombre d'arguments");
+                        System.exit(2);
+                    }
+                }
+            } else {
+                System.out.println("La fonction main n'est pas déclarée");
+                System.exit(2);
+            }
+        }
+        else {
+            System.out.println("La fonction " + node.getNom() + " n'est pas déclaré");
+            System.exit(2);
+        }
         defaultOut(node);
         return null;
     }
